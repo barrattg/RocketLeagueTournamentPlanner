c3b1ee9e4d3f396ef3cfe31e81e44831a4b89c76
Gregory Barratt <barrattg@hotmail.co.uk>
2020-05-30 20:20:01 +0100

    Class Models for basic function
    Created new class models for tracker:
    matchup entry model, matchup model, person model, prize model, team model, tournament model.

---
+++ Rocket League Tournament Planner (RLTP)/TrackerLibrary/TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {  
+        /// <summary>
+        /// Represents one team in the matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Score for particular team.
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// The previous match, that this team won in.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+      
+    }
+}
---
+++ Rocket League Tournament Planner (RLTP)/TrackerLibrary/TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {   
+
+        /// <summary>
+        /// Unique Name/Id for Tournament.
+        /// </summary>
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+
+        /// <summary>
+        /// List of all competing teams.
+        /// </summary>
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+
+        /// <summary>
+        /// How the prizes are allocated.
+        /// </summary>
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
+
+    }
+}
